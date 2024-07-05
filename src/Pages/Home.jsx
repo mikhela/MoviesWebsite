41e9aae608696881684279dddc966d970d29ea6f
unknown <giksona151@gmail.com>
2024-07-05 04:57:35 +0400

    new homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,33 +1,31 @@
-import React, { useState } from 'react';
-import "../styles/Home.css"
-import ErrorBoundary from '../components/ErrorBoundary'
-import HeaderInfo from '../components/HeaderInfo'
-import HeaderCarousel from '../components/HeaderCarousel'
-import HeaderBigBox from '../components/HeaderBigBox'
-import "../styles/HeaderResponsive.css"
+import React, { useState } from "react";
+import "../styles/Home.css";
+import ErrorBoundary from "../components/ErrorBoundary";
+import HeaderInfo from "../components/HeaderInfo";
+import HeaderCarousel from "../components/HeaderCarousel";
+import HeaderBigBox from "../components/HeaderBigBox";
+import "../styles/HeaderResponsive.css";
 
 const Home = () => {
-    const [boxClicked, setBoxClicked] = useState(false);
+  const [boxClicked, setBoxClicked] = useState(false);
 
-    const handleBoxClick = (clicked) => {
-        setBoxClicked(clicked);
-    };
-    return (
-        <>
-             <div className="header" >
-            <ErrorBoundary>
-                <div className="header__container">
-                    <div className={`innerOneContainer ${boxClicked ? "blurred" : ""}`}>
-                        <HeaderInfo />
-                        <HeaderCarousel />
-                    </div>
-                </div>
-                    <HeaderBigBox onBoxClick={handleBoxClick} />
+  const handleBoxClick = (clicked) => {
+    setBoxClicked(clicked);
+  };
+  return (
+    <>
+      <div className="header">
+        <ErrorBoundary>
+          <div className="header__container">
+            <div className={`innerOneContainer ${boxClicked ? "blurred" : ""}`}>
+              <HeaderInfo />
+            </div>
+          </div>
+          <HeaderBigBox onBoxClick={handleBoxClick} />
+        </ErrorBoundary>
+      </div>
+    </>
+  );
+};
 
-            </ErrorBoundary>
-        </div>
-        </>
-    );
-}
- 
 export default Home;
--- src/components/HeaderInfo.jsx
+++ src/components/HeaderInfo.jsx
@@ -1,14 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import useFetch from './useFetch';
-import '../styles/HeaderInfo.css'
+import React, { useEffect, useState } from "react";
+import useFetch from "./useFetch";
+import "../styles/HeaderInfo.css";
 
 function HeaderInfo() {
   const [movieIndex, setMovieIndex] = useState(0);
-  const { data: movies, isLoading, error } = useFetch('https://api.themoviedb.org/3/discover/movie?api_key=cd6592beb58e675d2cb6fdf038c87822');
+  const {
+    data: movies,
+    isLoading,
+    error,
+  } = useFetch(
+    "https://api.themoviedb.org/3/discover/movie?api_key=cd6592beb58e675d2cb6fdf038c87822"
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setMovieIndex(prevIndex => (prevIndex + 1) % movies.length);
+      setMovieIndex((prevIndex) => (prevIndex + 1) % movies.length);
     }, 3000);
 
     return () => clearInterval(interval);
@@ -25,15 +31,26 @@ function HeaderInfo() {
   const movie = movies[movieIndex];
   return (
     <div className="header--info">
-    <div className="information">
-      <h2 id='title'>{movie.title}</h2>
-      <p className='overview'>{movie.overview}</p>
-      <div className="innerBox">
-        <p id='vote'><span id='imdb'>IMDb</span> {movie.vote_average}</p>
-        <p>Popularity: {movie.popularity}</p>
+      <div className="left__image">
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          alt={movie.title}
+        />
+      </div>
+      <div className="information">
+        <div className="innerinformation">
+          <h2 id="title">{movie.title}</h2>
+          <p className="overview">{movie.overview}</p>
+
+          <div className="innerBox">
+            <p id="vote">
+              <span id="imdb">IMDb</span> {movie.vote_average}
+            </p>
+            <p>Popularity: {movie.popularity}</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
   );
 }
 
